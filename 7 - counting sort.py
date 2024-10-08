8c8962970552157fc604e509577aec9b102db9ad
HeeNamgoong <skarndgml2@kookmin.ac.kr>
2024-10-08 12:36:00 +0900

    counting sort comment

--- 7 - counting sort.py
+++ 7 - counting sort.py
@@ -2,15 +2,15 @@ def counting_sort(arr, r):
     n = len(arr)
     cnts = [0] * r
 
-    for x in arr:
+    for x in arr: # 각 값의 개수를 count
         cnts[x] += 1
 
-    for i in range(1, r):
+    for i in range(1, r): # cumulate, 값이 마지막으로 들어간 위치의 다음자리 index
         cnts[i] += cnts[i-1]
 
     sorted = [0] * n
     for x in reversed(arr):
-        cnts[x] -= 1
+        cnts[x] -= 1 # 배열에 삽입을 위해 값을 1씩 감소
         sorted[cnts[x]] = x
     
     arr[:] = sorted
